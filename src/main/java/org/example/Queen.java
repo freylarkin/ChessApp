4e2e5d5491374d398d7115c3ef52ca91745bae0e
freylarkin <114355394+freylarkin@users.noreply.github.com>
2024-08-13 19:50:39 -0400

    finished isValidMove() code

--- src/main/java/org/example/Queen.java
+++ src/main/java/org/example/Queen.java
@@ -8,11 +8,84 @@ public class Queen extends Piece {
 
     @Override
     public boolean isValidMove(int startX, int startY, int endX, int endY, ChessBoard board) {
-        return false;
+        // check for within bounds of board
+        if (endX < 0 || endX >= 8 || endY < 0 || endY >= 8) {
+            return false; // out of bounds
+        }
+
+        if (endX == startX) { // vertical move
+            if (endY > startY) { // moves down
+                for (int y = startY + 1; y <= endY; y++) {
+                    if (board.getPiece(startX, y) != null) {
+                        return false; // piece in the way
+                    }
+                }
+            } else { // moves up
+                for (int y = startY - 1; y >= endY; y--) {
+                    if (board.getPiece(startX, y) != null) {
+                        return false; // piece in the way
+                    }
+                }
+            }
+        } else if (endY == startY) { // horizontal move
+            if (endX > startX) { // moves right
+                for (int x = startX + 1; x <= endX; x++) {
+                    if (board.getPiece(x, startY) != null) {
+                        return false; // piece in the way
+                    }
+                }
+            } else { // moves left
+                for (int x = startX - 1; x >= endX; x--) {
+                    if (board.getPiece(x, startY) != null) {
+                        return false; // piece in the way
+                    }
+                }
+            }
+        } else if (Math.abs(endX - startX) == Math.abs(endY - startY)) {
+            // determine x direction
+            int xDirection;
+
+            if (endX > startX) {
+                xDirection = 1; // moves right
+            } else {
+                xDirection = -1; // moves left
+            }
+
+            // determine y direction
+            int yDirection;
+
+            if (endY > startY) {
+                yDirection = 1; // moves up
+            } else {
+                yDirection = -1; // moves down
+            }
+
+            int x = startX + xDirection;
+            int y = startY + yDirection;
+
+            // check if any pieces are in the way
+            while (x != endX && y != endY) {
+                if (board.getPiece(x, y) != null) {
+                    return false; // piece in the way
+                }
+                x += xDirection;
+                y += yDirection;
+            }
+        } else {
+            return false; // neither vertical nor horizontal nor diagonal
+        }
+
+        // check if destination is occupied by piece of same colour
+        if (isOccupiedBySameColour(board, endX, endY)) {
+            return false; // same colour piece in same spot
+        }
+        return true; // valid move
     }
 
     @Override
     public void move(int endX, int endY, ChessBoard board) {
-
+        if (isValidMove(this.getX(), this.getY(), endX, endY, board)) {
+            board.updateBoard(this, this.getX(), this.getY(), endX, endY);
+        }
     }
 }
