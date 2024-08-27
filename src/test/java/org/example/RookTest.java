5ff1352965eb972cc77dee1bc9af4f26721cb050
freylarkin <114355394+freylarkin@users.noreply.github.com>
2024-08-27 09:09:26 -0400

    re-init Rook class tests

--- src/test/java/org/example/RookTest.java
+++ src/test/java/org/example/RookTest.java
@@ -26,4 +26,54 @@ class RookTest {
     }
 
     @Test
-    void isO
+    void isOccupiedBySameColour_False() {
+        board.removePiece(0, 6); // remove anotherWhiteRook that's currently occupying this space
+        board.placePiece(whiteRook, 0, 5);
+        board.placePiece(blackRook, 0, 3);
+        assertFalse(whiteRook.isOccupiedBySameColour(board, 0, 3)); // check piece in front is diff colour
+
+    }
+
+    @Test
+    void isValidMove_VerticalMove() {
+        assertTrue(whiteRook.isValidMove(0, 5, 0, 2, board));
+        // accurately tested false the first time since there was a piece in the way
+    }
+
+    @Test
+    void isValidMove_HorizontalMove() {
+        assertTrue(whiteRook.isValidMove(0, 5, 5, 5, board));
+        // accurately tested false the first time since there was a piece in the way
+
+    }
+
+    @Test
+    void isValidMove_InvalidDiagonalMove() {
+        assertFalse(whiteRook.isValidMove(0, 7, 3, 4, board));
+    }
+
+    @Test
+    void isValidMove_CaptureMove() {
+        board.placePiece(whiteRook, 0, 5);
+        board.placePiece(blackRook, 0, 3);
+        assertTrue(whiteRook.isValidMove(0, 5, 0, 3, board)); // checks capture move is made
+    }
+
+    @Test
+    void capturePiece_ValidCapture() {
+        board.placePiece(whiteRook, 0, 5);
+        board.placePiece(blackRook, 0, 3);
+        whiteRook.move(0,3, board);
+        assertNull(board.getPiece(0, 5)); // check that og position is now empty
+        assertEquals(whiteRook, board.getPiece(0, 3)); // check rook is now in new position
+
+    }
+
+    @Test
+    void move_ValidMove() {
+        board.placePiece(whiteRook, 0, 5); // move the rook up since board initializes with all pieces
+        whiteRook.move(0,3, board);
+        assertNull(board.getPiece(0, 5)); // check og position is empty
+        assertEquals(whiteRook, board.getPiece(0, 3)); // check piece is at new location
+    }
+}
